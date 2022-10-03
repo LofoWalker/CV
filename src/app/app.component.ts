fc5f4c0b8659ffb71657d106b8123ee9a3afd002
Tom Walker <tom.walker@fabrick.io>
2022-10-03 15:02:53 +0200

    Work Well

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {faPhone, faMailBulk, faAddressBook, faBirthdayCake, faDownload} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  faPhone = faPhone;
+  faMail = faMailBulk;
+  faAddress = faAddressBook;
+  faBirth = faBirthdayCake;
+  faDownload = faDownload;
+
   title = 'CV';
+
+  download() {
+    console.log('Clicked');
+  }
 }
